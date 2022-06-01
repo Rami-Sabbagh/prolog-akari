f69338837f1452633b6a54a814eae9e24c3dcc39
Yaman Qassas <yaman102011@gmail.com>
2022-06-01 18:38:43 +0300

    Implement row_cells_until_wall

--- src/utils.pl
+++ src/utils.pl
@@ -7,3 +7,20 @@ adjacent_cells(cell(Row, Col), Cells4) :-
 	(Row > 1 -> Row2 is Row-1, append(Cells1, [cell(Row2, Col)], Cells2); Cells2 = Cells1),
 	(Col < Y -> Col1 is Col+1, append(Cells2, [cell(Row, Col1)], Cells3); Cells3 = Cells2),
 	(Col > 1 -> Col2 is Col-1, append(Cells3, [cell(Row, Col2)], Cells4); Cells4 = Cells3).
+
+row_cells_until_wall(cell(Row, Col), Cells) :-
+	right_cells_until_wall(cell(Row, Col), Right),
+	left_cells_until_wall(cell(Row, Col), Left),
+	append(Right, Left, Cells).
+
+right_cells_until_wall(cell(_, Col), []) :- size(_, Y), Col >= Y.
+right_cells_until_wall(cell(Row, Col), Cells) :-
+	Col2 is Col+1,
+	right_cells_until_wall(cell(Row, Col2), Cells2),
+	(wall(Row, Col2) -> Cells = []; Cells = [cell(Row,Col2) | Cells2]).
+
+left_cells_until_wall(cell(_, Col), []) :- Col =< 1.
+left_cells_until_wall(cell(Row, Col), Cells) :-
+	Col2 is Col-1,
+	left_cells_until_wall(cell(Row, Col2), Cells2),
+	(wall(Row, Col2) -> Cells = []; Cells = [cell(Row,Col2) | Cells2]).
