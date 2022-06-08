270cbd2b3f9e28c7f0e88556295b3e46a247d904
Rami Sabbagh <rami.sab07@gmail.com>
2022-06-08 22:26:12 +0300

    Implement `all_cells_lighted`

--- src/validation.pl
+++ src/validation.pl
@@ -3,6 +3,10 @@
 % ]).
 
 :- use_module(board).
+:- use_module(board_utils).
+:- use_module(dataset).
+
+:- reset_level,assert_nth0_puzzle(0).
 
 % solved:-
 %     all_cells_lighted,
@@ -13,6 +17,14 @@
 % all_cells_lighted %
 %-------------------%
 
+all_cells_lighted:-
+    size(Rows, Columns),
+    forall((between(1,Rows,R),between(1,Columns,C)), lighted(R,C)).
+
+lighted(R, C):- wall(R,C);light(R,C).
+lighted(R, C):-
+    light(RL, CL),
+    reachable(R,C, RL, CL).
 
 %-----------------%
 % no_double_light %
@@ -23,14 +35,31 @@
 % light_count_correct %
 %---------------------%
 
-%
+%-----------%
+% Utilities %
+%-----------%
 
 between_unorded(A, B, V):- between(A, B, V);between(B, A, V).
 
-unreachable(R1,C1, R2,C2):-
+% unreachable(R1,C1, R2,C2):-
+%     size(Rows, Columns),
+
+%     (R1 = R2; C1 = C2),
+
+%     between(1, Rows, R1),
+%     between(1, Columns, C1),
+
+%     between(1, Rows, R2),
+%     between(1, Columns, C2),
+
+%     between_unorded(R1, R2, R),
+%     between_unorded(C1, C2, C),
+%     wall(R, C).
+
+reachable(R1,C1, R2,C2):-
     size(Rows, Columns),
 
-    (R1 =:= R2; C1 =:= C2),
+    (R1 = R2; C1 = C2),
 
     between(1, Rows, R1),
     between(1, Columns, C1),
@@ -38,11 +67,8 @@ unreachable(R1,C1, R2,C2):-
     between(1, Rows, R2),
     between(1, Columns, C2),
 
-    wall(R, C),
     between_unorded(R1, R2, R),
-    between_unorded(C1, C2, C).
-
-reachable(R1,C1, R2,C2):-
-    \+ unreachable(R1,C1, R2,C2).
+    between_unorded(C1, C2, C),
+    \+ wall(R, C).
 
 
