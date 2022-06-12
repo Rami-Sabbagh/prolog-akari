663d25870f190e2bed3498f2f92c0c9ead4760a0
Rami Sabbagh <rami.sab07@gmail.com>
2022-06-12 19:44:38 +0300

    Add 2 handwritten tests
    Which test some edge cases in lighting the remaining cells

--- src/tests_full.pl
+++ src/tests_full.pl
@@ -2754,3 +2754,5 @@ test('KD_Akari_20x20_V1-B58-P15',[setup(assert_nth0_puzzle(2752)), cleanup(reset
 test('KD_Akari_20x20_V1-B58-P16',[setup(assert_nth0_puzzle(2753)), cleanup(reset_level)]):- test_puzzle,!.
 test('KD_Akari_20x20_V1-B59-P1',[setup(assert_nth0_puzzle(2754)), cleanup(reset_level)]):- test_puzzle,!.
 test('KD_Akari_20x20_V1-B59-P2',[setup(assert_nth0_puzzle(2755)), cleanup(reset_level)]):- test_puzzle,!.
+test('RAMI_5x5_V10-B1-P1',[setup(assert_nth0_puzzle(2756)), cleanup(reset_level)]):- test_puzzle,!.
+test('RAMI_5x5_V10-B1-P2',[setup(assert_nth0_puzzle(2757)), cleanup(reset_level)]):- test_puzzle,!.
--- src/tests_picked.pl
+++ src/tests_picked.pl
@@ -1,4 +1,7 @@
 test('KD_Akari_8x8_V1-B1-P1',[setup(assert_nth0_puzzle(0)), cleanup(reset_level)]):- test_puzzle,!.
+test('KD_Akari_8x8_V1-B76-P13',[setup(assert_nth0_puzzle(1812)), cleanup(reset_level)]):- test_puzzle,!.
 test('KD_Akari_8x8_V1-B77-P2',[setup(assert_nth0_puzzle(1825)), cleanup(reset_level)]):- test_puzzle,!.
 test('KD_Akari_20x20_V1-B1-P1',[setup(assert_nth0_puzzle(1826)), cleanup(reset_level)]):- test_puzzle,!.
 test('KD_Akari_20x20_V1-B59-P2',[setup(assert_nth0_puzzle(2755)), cleanup(reset_level)]):- test_puzzle,!.
+test('RAMI_5x5_V10-B1-P1',[setup(assert_nth0_puzzle(2756)), cleanup(reset_level)]):- test_puzzle,!.
+test('RAMI_5x5_V10-B1-P2',[setup(assert_nth0_puzzle(2757)), cleanup(reset_level)]):- test_puzzle,!.
