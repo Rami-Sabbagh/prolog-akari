f6d5e878742122ab960767ea1093d000b9dc298a
Rami Sabbagh <rami.sab07@gmail.com>
2022-06-14 00:31:49 +0300

    Syntax sugar

--- src/utils.pl
+++ src/utils.pl
@@ -6,18 +6,20 @@
     adjacent_cell/4,
     adjacent_cells/3,
     
-    reachable/4,
-    % reach_dir(R,C, RT,CT).
+    reachable/4, % alias for reachable(any, R1,C1, R2,C2).
+    reachable/5
+    % reachable(direction, R,C, RT,CT).
     % - [R,C] for the cell you are at.
     % - [RT,CT] is the result cell.
+    % - direction can be: up, down, left, right, row, column, any.
     % examples:
-    reach_up/4, % - reach_up(5,5, 4,5).
-    reach_down/4, % - reach_down(5,5, 6,5).
-    reach_left/4, % - reach_left(5,5, 5,4).
-    reach_right/4, % - reach_right(5,5, 5,6).
-
-    reach_horizontal/4,
-    reach_vertical/4
+    % - reach(up, 5,5, 4,5).
+    % - reach(down, 5,5, 6,5).
+    % - reach(left, 5,5, 5,4).
+    % - reach(right, 5,5, 5,6).
+    % - reach(row, 5,5, 5,6).
+    % - reach(column, 5,5, 6,5).
+    % - reach(any, 5,5, 6,5).
 ]).
 
 :- use_module(board).
@@ -36,30 +38,28 @@ in_board(R, C):-
     between(1, Columns, C).
 
 % complexity: O(N) where N is the board dimension.
-reach_up(R,C, RT,C):- RT is R-1, RT > 0, \+ wall(RT,C).
-reach_up(R,C, RT,C):- R2 is R-1, R2 > 1, \+ wall(R2,C), reach_up(R2,C, RT,C).
+reachable(up, R,C, RT,C):- RT is R-1, RT > 0, \+ wall(RT,C).
+reachable(up, R,C, RT,C):- R2 is R-1, R2 > 1, \+ wall(R2,C), reachable(up, R2,C, RT,C).
 
 % complexity: O(N) where N is the board dimension.
-reach_left(R,C, R,CT):- CT is C-1, CT > 0, \+ wall(R,CT).
-reach_left(R,C, R,CT):- C2 is C-1, C2 > 1, \+ wall(R,C2), reach_left(R,C2, R,CT).
+reachable(left, R,C, R,CT):- CT is C-1, CT > 0, \+ wall(R,CT).
+reachable(left, R,C, R,CT):- C2 is C-1, C2 > 1, \+ wall(R,C2), reachable(left, R,C2, R,CT).
 
 % complexity: O(N) where N is the board dimension.
-reach_down(R,C, RT,C):- size(MAX,_), RT is R+1, RT =< MAX, \+ wall(RT,C).
-reach_down(R,C, RT,C):- size(MAX,_), R2 is R+1, R2 < MAX, \+ wall(R2,C), reach_down(R2,C, RT,C).
+reachable(down, R,C, RT,C):- size(MAX,_), RT is R+1, RT =< MAX, \+ wall(RT,C).
+reachable(down, R,C, RT,C):- size(MAX,_), R2 is R+1, R2 < MAX, \+ wall(R2,C), reachable(down, R2,C, RT,C).
 
 % complexity: O(N) where N is the board dimension.
-reach_right(R,C, R,CT):- size(_,MAX), CT is C+1, CT =< MAX, \+ wall(R,CT).
-reach_right(R,C, R,CT):- size(_,MAX), C2 is C+1, C2 < MAX, \+ wall(R,C2), reach_right(R,C2, R,CT).
+reachable(right, R,C, R,CT):- size(_,MAX), CT is C+1, CT =< MAX, \+ wall(R,CT).
+reachable(right, R,C, R,CT):- size(_,MAX), C2 is C+1, C2 < MAX, \+ wall(R,C2), reachable(right, R,C2, R,CT).
 
 % complexity: O(N) where N is the board dimension.
-reach_horizontal(R,C, RT,CT):- in_board(R,C),(reach_left(R,C,RT,CT);reach_right(R,C,RT,CT)).
+reachable(row, R,C, RT,CT):- in_board(R,C),(reachable(left, R,C,RT,CT);reachable(right, R,C,RT,CT)).
 % complexity: O(N) where N is the board dimension.
-reach_vertical(R,C, RT,CT):- in_board(R,C),(reach_up(R,C,RT,CT);reach_down(R,C,RT,CT)).
+reachable(column, R,C, RT,CT):- in_board(R,C),(reachable(up, R,C,RT,CT);reachable(down, R,C,RT,CT)).
 
 /* complexity:
-
-O(N) when the 2 cells are pinned.
-O(N) when a cell is pinned.
+O(N) when first cell is pinned.
 O(N^2) when no cells are pinned.
 
 where N is the board dimension.
@@ -67,7 +67,8 @@ where N is the board dimension.
 
 % checks or searches for cells that have a clear line of sight.
 % so if one of them was a light, it's ray would reach the other cell.
-reachable(R1,C1, R2,C2):-  in_board(R1,C1),(reach_horizontal(R1,C1, R2,C2);reach_vertical(R1,C1, R2,C2)).
+reachable(any, R1,C1, R2,C2):- in_board(R1,C1),(reachable(row ,R1,C1, R2,C2);reachable(column, R1,C1, R2,C2)).
+reachable(R1,C1, R2,C2):- reachable(any, R1,C1, R2,C2).
 
 % complexity: O(N^2).
 %
