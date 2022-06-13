3ce5143873bf2653219b3c5f39b5adcd3bd836f1
Rami Sabbagh <rami.sab07@gmail.com>
2022-06-13 22:11:04 +0300

    Simplify `adjacent_cells/2`

--- src/solution.pl
+++ src/solution.pl
@@ -6,14 +6,8 @@
 :- use_module(utils).
 
 % Get adjacent cells of a given cell
-adjacent_cells(cell(Row, Col), Cells4) :-
-	Cells0 = [],
-	size(X, Y),
-	(Row < X -> Row1 is Row+1, append(Cells0, [cell(Row1, Col)], Cells1); Cells1 = Cells0),
-	(Row > 1 -> Row2 is Row-1, append(Cells1, [cell(Row2, Col)], Cells2); Cells2 = Cells1),
-	(Col < Y -> Col1 is Col+1, append(Cells2, [cell(Row, Col1)], Cells3); Cells3 = Cells2),
-	(Col > 1 -> Col2 is Col-1, append(Cells3, [cell(Row, Col2)], Cells4); Cells4 = Cells3).
-
+adjacent_cells(cell(R, C), Cells) :-
+	findall(cell(RA,CA), adjacent_cell(R,C, RA,CA), Cells).
 
 % Get all cells on the right and left of the given cell,
 % until a wall, or the edge of the board is reached.
